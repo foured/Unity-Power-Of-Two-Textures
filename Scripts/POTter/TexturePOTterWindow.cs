4f29f6eb8d75f34401c6bffa38405822b202facd
Ivan <100620835+foured@users.noreply.github.com>
2024-07-26 22:15:24 +0300

    Update TexturePOTterWindow.cs

--- Scripts/POTter/TexturePOTterWindow.cs
+++ Scripts/POTter/TexturePOTterWindow.cs
@@ -4,12 +4,19 @@ using System.IO;
 
 public class TexturePOTterWindow : EditorWindow
 {
+    public enum Mode
+    {
+        CreateNew,
+        OverrideThis
+    }
+
     private const string DefaultNewTextureName = "'texture name'_POT";
     private string _textureName = DefaultNewTextureName;
     private string _path = Application.dataPath;
     private string _outputMSG = "";
     private Texture2D _texture;
     private bool _copyData = true;
+    private Mode _mode = Mode.CreateNew;
 
     [MenuItem("Window/TexturePOTter")]
     public static void ShowWindow()
@@ -19,36 +26,44 @@ public class TexturePOTterWindow : EditorWindow
 
     private void OnGUI()
     {
-        GUILayout.Label("Texture save folder:", EditorStyles.boldLabel);
+        _mode = (Mode)EditorGUILayout.EnumPopup("Mode", _mode);
 
-        GUILayout.BeginHorizontal();
+        if (_mode == Mode.CreateNew)
         {
-            GUILayout.Label(_path, EditorStyles.label);
-            if (GUILayout.Button("Browse", GUILayout.Width(100f)))
+            GUILayout.Label("Texture save folder:", EditorStyles.boldLabel);
+
+            GUILayout.BeginHorizontal();
             {
-                _path = EditorUtility.OpenFolderPanel("TexturePOTter", Application.dataPath, "");
+                GUILayout.Label(_path, EditorStyles.label);
+                if (GUILayout.Button("Browse", GUILayout.Width(100f)))
+                {
+                    _path = EditorUtility.OpenFolderPanel("TexturePOTter", Application.dataPath, "");
+                }
             }
+            GUILayout.EndHorizontal();
+
+            _textureName = EditorGUILayout.TextField("New texture name", _textureName);
+            _copyData = EditorGUILayout.Toggle("Copy texture settings", _copyData);
         }
-        GUILayout.EndHorizontal();
 
-        _textureName = EditorGUILayout.TextField("New texture name", _textureName);
-        _copyData = EditorGUILayout.Toggle("Copy texture settings", _copyData);
         _texture = (Texture2D)EditorGUILayout.ObjectField(_texture, typeof(Texture2D), false);
-
         GUILayout.Space(10);
 
-        if (GUILayout.Button("Rest folder"))
+        if (_mode == Mode.CreateNew)
         {
-            _path = Application.dataPath;
-            _outputMSG = "Folder rested";
-        }
 
-        if (GUILayout.Button("Rest name"))
-        {
-            _textureName = DefaultNewTextureName;
-            _outputMSG = "Name rested";
-        }
+            if (GUILayout.Button("Rest folder"))
+            {
+                _path = Application.dataPath;
+                _outputMSG = "Folder rested";
+            }
 
+            if (GUILayout.Button("Rest name"))
+            {
+                _textureName = DefaultNewTextureName;
+                _outputMSG = "Name rested";
+            }
+        }
         bool isReadable = _texture == null ? false : _texture.isReadable;
 
         if (GUILayout.Button($"Set read/write to {!isReadable}"))
@@ -76,10 +91,18 @@ public class TexturePOTterWindow : EditorWindow
                 TexturePOTter potter = new TexturePOTter(_texture);
                 if (potter.SetTextureData())
                 {
-                    string tn = _textureName == DefaultNewTextureName ? null : _textureName;
-                    _outputMSG = potter.Process(_path, tn);
-                    if (_copyData)
-                        potter.CopyData();
+                    if (_mode == Mode.CreateNew)
+                    {
+                        string tn = _textureName == DefaultNewTextureName ? null : _textureName;
+                        _outputMSG = potter.ProcessNew(_path, tn);
+                        if (_copyData)
+                            potter.CopyData();
+                    }
+                    else
+                    {
+                        Undo.RegisterCompleteObjectUndo(_texture, "Modified texture");
+                        _outputMSG = potter.ProcessCurrent();
+                    }
                     AssetDatabase.Refresh();
                 }
                 else
